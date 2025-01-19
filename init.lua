578cecda7322c7e47d2f5340a10289a9de228cff
rxt30 <mhampel@pm.me>
2025-01-19 16:50:28 +0100

    Added project.nvim

--- init.lua
+++ init.lua
@@ -951,6 +951,12 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'ahmedkhalf/project.nvim',
+    config = function()
+      require('project_nvim').setup {}
+    end,
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
